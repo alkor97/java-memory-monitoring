916607d2862c21d7e5947d3018cc359e29c45ea4
stachurs <ext-maciej.stachurski@here.com>
2019-11-15 13:21:41 +0100

    Added log downloading script.

---
+++ scripts/download-logs.sh
@@ -0,0 +1,11 @@
+
+SERVER=$1
+PID=$2
+
+HOST=${!SERVER}
+DIR=./histo/${SERVER}
+
+mkdir -p ${DIR}
+echo ${DIR}
+scp ${HOST}:/tmp/my-heap-${PID}.log ${DIR}
+scp ${HOST}:/tmp/my-histo-${PID}.log ${DIR}
