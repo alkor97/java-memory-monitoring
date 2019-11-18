fd35a55bb4d0dc341c90279ac6d1022ded7a6a7f
stachurs <ext-maciej.stachurski@here.com>
2019-11-18 10:23:48 +0100

    Script error fixed

--- scripts/log-jvm-info.sh
+++ scripts/log-jvm-info.sh
@@ -9,7 +9,7 @@ HEAP_LOG=/tmp/my-heap-${PID}.log
 HISTO_LOG=/tmp/my-histo-${PID}.log
 
 printf "\n*** ${TIMESTAMP} ***\n" >>${HEAP_LOG}
-${ELEVATION} navteq jmap -heap ${PID} >>${HEAP_LOG}
+${ELEVATION} jmap -heap ${PID} >>${HEAP_LOG}
 
 printf "\n*** ${TIMESTAMP} ***\n" >>${HISTO_LOG}
 ${ELEVATION} jmap -histo ${PID} >>${HISTO_LOG}
